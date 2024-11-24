295e4b9754f9f242e3ae553afd156d9c579dc297
Mihail <otel1994@yahoo.com>
2024-11-24 21:19:52 +0200

    Homework 9

--- src/main/java/homework_nr_6/BankAccount.java
+++ src/main/java/homework_nr_6/BankAccount.java
@@ -53,5 +53,6 @@ public class BankAccount {
         System.out.println("-----------------------------------------------------------");
         System.out.println("Current balance after all operations: "+ BankAccount.balance);
         }
+        //
     }
 
